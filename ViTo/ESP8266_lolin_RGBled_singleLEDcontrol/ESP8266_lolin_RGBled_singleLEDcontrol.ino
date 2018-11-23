dd4be8f920001469e39dad776ac72b2fe531f3ac
lum1pyry12122017 <34477046+lum1pyry12122017@users.noreply.github.com>
2018-11-23 14:06:54 +0200

    Add files via upload

--- ViTo/ESP8266_lolin_RGBled_singleLEDcontrol/ESP8266_lolin_RGBled_singleLEDcontrol.ino
+++ ViTo/ESP8266_lolin_RGBled_singleLEDcontrol/ESP8266_lolin_RGBled_singleLEDcontrol.ino
@@ -1,14 +1,37 @@
 #include "Arduino.h"
 #include "lolin_RGBled.h"
+#include "Settings.h"
 
 void setup() {
+  Serial.begin(9600);
+//  addVersionToSerialOutput();
 }
 
-RgbLedControl control();
+RgbLedControl control;
 
 void loop()
 {
-//  control.dummy();
+  control.dummy();
+  control.RgbLedControlFunction(0,10,0,0);
+  control.RgbLedControlFunction(1,20,0,0);
+//  control.RgbLedControlFunction(2,0,10,0);
+//  control.RgbLedControlFunction(3,0,20,0);
+//  control.RgbLedControlFunction(4,0,0,10);
+//  control.RgbLedControlFunction(5,0,0,20);
+//  control.RgbLedControlFunction(6,10,10,10);
+  delay(1000);
 }
-  
- 
+
+void addVersionToSerialOutput()
+{
+    char fullPath[] = __FILE__;
+    char *slash;
+    slash = strrchr(fullPath, '/');
+    slash++;
+    strcpy(fullPath, slash);
+    byte length = strlen(fullPath);
+    char fileName[20] = "";
+    strncpy(fileName, fullPath, (length - 4));
+    strcat(fileName, ".ino");
+    Serial.print(fileName);
+}
---
+++ ViTo/ESP8266_lolin_RGBled_singleLEDcontrol/Settings.h
@@ -0,0 +1,15 @@
+#ifndef lolin_RGBled
+#define lolin_RGBled
+/*
+    char fullPath[] = __FILE__;
+    char *slash;
+    slash = strrchr(fullPath, '/');
+    slash++;
+    strcpy(fullPath, slash);
+    byte length = strlen(fullPath);
+    char fileName[20] = "";
+    strncpy(fileName, fullPath, (length - 4));
+    strcat(fileName, ".ino");
+    Serial.print(fileName);
+*/
+#endif
